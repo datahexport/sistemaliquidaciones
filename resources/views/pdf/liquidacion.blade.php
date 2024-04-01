5de3571ec1cd6163915c5dcab900c56b7c8d9c54
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-04-01 13:07:18 -0300

    u

--- resources/views/pdf/liquidacion.blade.php
+++ resources/views/pdf/liquidacion.blade.php
@@ -113,18 +113,18 @@
 		
 		<table id="balance" style="width:100%; border-collapse: collapse; margin-top: 20px;">
 			<thead>
-			  <tr >
+			  <tr style="border: 1px solid black; font-weight: bold;">
 				
-				<th style="border: 1px solid black;">KG<br>Variedad</th>
+				<th style="border: 1px solid black; font-weight: bold;   ">KG<br>Variedad</th>
 				
-				<th style="border: 1px solid black;">Tipo<br>Exportación</th>
+				<th>Tipo<br>Exportación</th>
 				
 			
-				<th style="border: 1px solid black;">Comercial</th>
-				<th style="border: 1px solid black;">PRE-CALIBRE</th>
-				<th style="border: 1px solid black;">MERMA DESECHO</th>
-				<th style="border: 1px solid black;">MERMA HOJAS</th>
-				<th style="border: 1px solid black;">TOTAL</th>
+				<th>Comercial</th>
+				<th>PRE-CALIBRE</th>
+				<th>MERMA DESECHO</th>
+				<th>MERMA HOJAS</th>
+				<th>TOTAL</th>
 				
 			  </tr>
 			</thead>
@@ -254,13 +254,13 @@
 							<tr>
 								
 								
-								<td style="border: 1px solid black;">{{$variedad}}</td>
-								<td style="border: 1px solid black;">{{number_format($pesonetoxl)}}</td>
-								<td style="border: 1px solid black;">{{number_format($pesoneto4j)}}</td>
-								<td style="border: 1px solid black;">{{number_format($pesoneto3j)}}</td>
-								<td style="border: 1px solid black;">{{number_format($pesoneto2j)}}</td>
-								<td style="border: 1px solid black;">{{number_format($pesonetoj)}}</td>
-								<td style="border: 1px solid black;">{{number_format($pesonetoxl+$pesoneto4j+$pesoneto3j+$pesoneto2j+$pesonetoj)}}</td>
+								<td style="border-top: 0.5px groove black;">{{$variedad}}</td>
+								<td style="border-top: 0.5px groove black;">{{number_format($pesonetoxl)}}</td>
+								<td style="border-top: 0.5px groove black;">{{number_format($pesoneto4j)}}</td>
+								<td style="border-top: 0.5px groove black;">{{number_format($pesoneto3j)}}</td>
+								<td style="border-top: 0.5px groove black;">{{number_format($pesoneto2j)}}</td>
+								<td style="border-top: 0.5px groove black;">{{number_format($pesonetoj)}}</td>
+								<td style="border-top: 0.5px groove black;">{{number_format($pesonetoxl+$pesoneto4j+$pesoneto3j+$pesoneto2j+$pesonetoj)}}</td>
 
 								
 								
@@ -281,16 +281,16 @@
 
 				@endforeach
 
-				<tr>
+				<tr style="border: 1px solid black; padding-bottom: 4px; margin-top: 10px; font-weight: bold;">
 							
 					
 					<td style="border: 1px solid black; padding-bottom: 4px; margin-top: 10px; font-weight: bold;">Total</td>
-					<td style="border: 1px solid black; padding-bottom: 4px; margin-top: 10px; font-weight: bold;">{{number_format($totalpesonetoxl)}} KG</td>
-					<td style="border: 1px solid black; padding-bottom: 4px; margin-top: 10px; font-weight: bold;">{{number_format($totalpesoneto4j)}} KG</td>
-					<td style="border: 1px solid black; padding-bottom: 4px; margin-top: 10px; font-weight: bold;">{{number_format($totalpesoneto3j)}} KG</td>
-					<td style="border: 1px solid black; padding-bottom: 4px; margin-top: 10px; font-weight: bold;">{{number_format($totalpesoneto2j)}} KG</td>
-					<td style="border: 1px solid black; padding-bottom: 4px; margin-top: 10px; font-weight: bold;">{{number_format($totalpesonetoj)}} KG</td>
-					<td style="border: 1px solid black; padding-bottom: 4px; margin-top: 10px; font-weight: bold;">{{number_format($totalpesonetoxl+$totalpesoneto4j+$totalpesoneto3j+$totalpesoneto2j+$totalpesonetoj)}} KG</td>
+					<td>{{number_format($totalpesonetoxl)}} KG</td>
+					<td>{{number_format($totalpesoneto4j)}} KG</td>
+					<td>{{number_format($totalpesoneto3j)}} KG</td>
+					<td>{{number_format($totalpesoneto2j)}} KG</td>
+					<td>{{number_format($totalpesonetoj)}} KG</td>
+					<td>{{number_format($totalpesonetoxl+$totalpesoneto4j+$totalpesoneto3j+$totalpesoneto2j+$totalpesonetoj)}} KG</td>
 			
 				</tr>
 						
