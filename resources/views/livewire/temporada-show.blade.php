02472f98e825296ef3222b52b45344ed85a04a36
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-04-16 13:44:08 -0400

    i

--- resources/views/livewire/temporada-show.blade.php
+++ resources/views/livewire/temporada-show.blade.php
@@ -223,6 +223,7 @@
                                                 {{number_format($cajasbulto)}}
                                             </div>    
                                         </td>
+                                        
                                         <td class="px-6 py-0 whitespace-nowrap">
                                           <div class="text-sm text-gray-900">{{number_format($pesoneto)}}</div>    
                                         </td>
