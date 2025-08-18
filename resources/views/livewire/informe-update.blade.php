2bb5d9b8fe372952e69e13c89c97a43a3089d3a2
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2025-08-18 15:49:11 -0400

    u

--- resources/views/livewire/informe-update.blade.php
+++ resources/views/livewire/informe-update.blade.php
@@ -168,6 +168,7 @@
               if ($item->fob) {
                 $tarifafinal=0;
                 $tarifafinal2=0;
+                $$tarifafinal3=0;
                 if ($item->fob->tarifas->count()>0) {
                     $tarifafinal=$item->fob->fob_kilo_salida;
 
